1c608d45e8934ead5540643853a90abce1b9aefd
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-04 00:03:54 +0200

    Add All Configs

---
+++ src/DAL/Configs/MaterialConfig.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class MaterialConfig : IEntityTypeConfiguration<Material>
+    {
+        public void Configure(EntityTypeBuilder<Material> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name);
+        }
+    }
+}
---
+++ src/DAL/Configs/PlaceConfig.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class PlaceConfig : IEntityTypeConfiguration<Place>
+    {
+        public void Configure(EntityTypeBuilder<Place> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Town).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Longitude).IsRequired();
+            builder.Property(x => x.Latitude).IsRequired();
+        }
+    }
+}
