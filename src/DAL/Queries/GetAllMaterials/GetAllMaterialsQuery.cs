01449c2636af388ebcbc4eeadbc988738df5c25d
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-05 00:02:56 +0200

    Add GetAllMaterials Query and QueryHandler

---
+++ src/DAL/Queries/GetAllMaterials/GetAllMaterialsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entity;
+using Core.Queries;
+
+namespace DAL.Queries.GetAllMaterials
+{
+    public class GetAllMaterialsQuery : IQuery<IList<Material>>
+    {
+    }
+}
---
+++ src/DAL/Queries/GetAllMaterials/GetAllMaterialsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entity;
+using Core.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Queries.GetAllMaterials
+{
+    public class GetAllMaterialsQueryHandler : IQueryHandler<GetAllMaterialsQuery, IList<Material>>
+    {
+        private readonly BuildingContext _buildingContext;
+        public GetAllMaterialsQueryHandler(BuildingContext buildingContext)
+        {
+            _buildingContext = buildingContext;
+        }
+        public async Task<IList<Material>> HandleAsync(GetAllMaterialsQuery query, CancellationToken cancellationToken = default)
+        {
+            List<Material> materials = await _buildingContext.Materials.ToListAsync(cancellationToken);
+
+            return materials;
+        }
+    }
+}
