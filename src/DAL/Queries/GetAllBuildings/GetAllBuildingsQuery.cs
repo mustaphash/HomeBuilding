b4b6df99e62f2ffba64c850ab67ba7b6400fc7c8
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-04 23:36:26 +0200

    Add GetAllBuildings Query and QueryHandler

---
+++ src/DAL/Queries/GetAllBuildings/GetAllBuildingsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entity;
+using Core.Queries;
+
+namespace DAL.Queries.GetAllBuildings
+{
+    public class GetAllBuildingsQuery : IQuery<IList<Building>>
+    {
+    }
+}
---
+++ src/DAL/Queries/GetAllBuildings/GetAllBuildingsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entity;
+using Core.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Queries.GetAllBuildings
+{
+    public class GetAllBuildingsQueryHandler : IQueryHandler<GetAllBuildingsQuery, IList<Building>>
+    {
+        private readonly BuildingContext _buildingContext;
+        public GetAllBuildingsQueryHandler(BuildingContext buildingContext)
+        {
+            _buildingContext = buildingContext;
+        }
+        public async Task<IList<Building>> HandleAsync(GetAllBuildingsQuery query, CancellationToken cancellationToken = default)
+        {
+            List<Building> buildings = await _buildingContext.Buildings.ToListAsync(cancellationToken);
+
+            return buildings;
+        }
+    }
+}
