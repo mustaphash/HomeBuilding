3ad51d55d0469dd62077ef8c32f9740cae93fe45
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-05 00:06:42 +0200

    Add GetAllPlaces Query and QUeryHandler

---
+++ src/DAL/Queries/GetAllPlaces/GetAllPlacesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entity;
+using Core.Queries;
+
+namespace DAL.Queries.GetAllPlaces
+{
+    public class GetAllPlacesQuery : IQuery<IList<Place>>
+    {
+    }
+}
---
+++ src/DAL/Queries/GetAllPlaces/GetAllPlacesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entity;
+using Core.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Queries.GetAllPlaces
+{
+    public class GetAllPlacesQueryHandler : IQueryHandler<GetAllPlacesQuery, IList<Place>>
+    {
+        private readonly BuildingContext _buildingContext;
+        public GetAllPlacesQueryHandler(BuildingContext buildingContext)
+        {
+            _buildingContext = buildingContext;
+        }
+        public async Task<IList<Place>> HandleAsync(GetAllPlacesQuery query, CancellationToken cancellationToken = default)
+        {
+            List<Place> places = await _buildingContext.Places.ToListAsync(cancellationToken);
+
+            return places;
+        }
+    }
+}
