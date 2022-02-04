d9cc64f5582658b021bee586266c113d582161fa
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-05 00:17:06 +0200

    Add BrigadeController

---
+++ src/HomeBuild/Controllers/BrigadeController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entity;
+using Core.Queries;
+using DAL.Queries.GetAllBrigades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBuild.Controllers
+{
+    [Route("brigades")]
+    [ApiController]
+    public class BrigadeController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllBrigadesQuery, IList<Brigade>> _getAllBrigadesQuery;
+        public BrigadeController(
+            IQueryHandler<GetAllBrigadesQuery, IList<Brigade>> getAllBrigadesQuery)
+        {
+            _getAllBrigadesQuery = getAllBrigadesQuery;
+        }
+
+        [HttpGet(Name = "GetAllBrigades")]
+        public async Task<IActionResult> GetAllBrigades()
+        {
+            IList<Brigade> brigades = await _getAllBrigadesQuery.HandleAsync(new GetAllBrigadesQuery());
+
+            return Ok(brigades);
+        }
+    }
+}
