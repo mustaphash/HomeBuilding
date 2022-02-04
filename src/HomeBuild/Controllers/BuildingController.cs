5b27796e30d45a50724071b02c8daf197ecbfce5
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-05 00:26:17 +0200

    Add Building and Employee Controller

---
+++ src/HomeBuild/Controllers/BuildingController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entity;
+using Core.Queries;
+using DAL.Queries.GetAllBuildings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBuild.Controllers
+{
+    [Route("buildings")]
+    [ApiController]
+    public class BuildingController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllBuildingsQuery, IList<Building>> _getAllBuildingsQuery;
+        public BuildingController(
+            IQueryHandler<GetAllBuildingsQuery, IList<Building>> getAllBuildingsQuery)
+        {
+            _getAllBuildingsQuery = getAllBuildingsQuery;
+        }
+
+        [HttpGet(Name = "GetAllBuildings")]
+        public async Task<IActionResult> GetAllBuildings()
+        {
+            IList<Building> buildings = await _getAllBuildingsQuery.HandleAsync(new GetAllBuildingsQuery());
+
+            return Ok(buildings);
+        }
+    }
+}
---
+++ src/HomeBuild/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entity;
+using Core.Queries;
+using DAL.Queries.GetAllEmployees;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBuild.Controllers
+{
+    [Route("employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllEmployeesQuery, IList<Employee>> _getAllEmployeesQuery;
+        public EmployeeController(
+            IQueryHandler<GetAllEmployeesQuery, IList<Employee>> getAllEmployeesQuery)
+        {
+            _getAllEmployeesQuery = getAllEmployeesQuery;
+        }
+
+        [HttpGet(Name = "GetAllEmployees")]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            IList<Employee> employees = await _getAllEmployeesQuery.HandleAsync(new GetAllEmployeesQuery());
+
+            return Ok(employees);
+        }
+    }
+}
