206f6a603d8d8da7b4e745a8c10371ca6bc1b1eb
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-05 00:35:31 +0200

    Add Material and Place Controller

---
+++ src/HomeBuild/Controllers/MaterialController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entity;
+using Core.Queries;
+using DAL.Queries.GetAllMaterials;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBuild.Controllers
+{
+    [Route("materials")]
+    [ApiController]
+    public class MaterialController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllMaterialsQuery, IList<Material>> _getAllMaterialsQuery;
+        public MaterialController(
+            IQueryHandler<GetAllMaterialsQuery, IList<Material>> getAllMaterialsQuery)
+        {
+            _getAllMaterialsQuery = getAllMaterialsQuery;
+        }
+
+        [HttpGet(Name = "GetAllMaterials")]
+        public async Task<IActionResult> GetAllMaterials()
+        {
+            IList<Material> materials = await _getAllMaterialsQuery.HandleAsync(new GetAllMaterialsQuery());
+
+            return Ok(materials);
+        }
+    }
+}
---
+++ src/HomeBuild/Controllers/PlaceController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entity;
+using Core.Queries;
+using DAL.Queries.GetAllPlaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBuild.Controllers
+{
+    [Route("places")]
+    [ApiController]
+    public class PlaceController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllPlacesQuery, IList<Place>> _getAllPlaceQuery;
+        public PlaceController(
+            IQueryHandler<GetAllPlacesQuery, IList<Place>> getAllPlaceQuery)
+        {
+            _getAllPlaceQuery = getAllPlaceQuery;
+        }
+
+        [HttpGet(Name = "GetAllPlaces")]
+        public async Task<IActionResult> GetAllPlaces()
+        {
+            IList<Place> places = await _getAllPlaceQuery.HandleAsync(new GetAllPlacesQuery());
+
+            return Ok(places);
+        }
+    }
+}
