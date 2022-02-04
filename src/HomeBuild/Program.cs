3884cec227d3f0344fe7be62953d2abbffa4c664
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-05 00:53:16 +0200

    Create DataBase

--- src/HomeBuild/Program.cs
+++ src/HomeBuild/Program.cs
@@ -1,3 +1,13 @@
+using Core.Entity;
+using Core.Queries;
+using DAL;
+using DAL.Queries.GetAllBrigades;
+using DAL.Queries.GetAllBuildings;
+using DAL.Queries.GetAllEmployees;
+using DAL.Queries.GetAllMaterials;
+using DAL.Queries.GetAllPlaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BuildingContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStrings")));
+
+builder.Services.AddScoped<IQueryHandler<GetAllBrigadesQuery, IList<Brigade>>, GetAllBrigadesQueryHandler>();
+builder.Services.AddScoped<IQueryHandler<GetAllBuildingsQuery, IList<Building>>, GetAllBuildingsQueryHandler>();
+builder.Services.AddScoped<IQueryHandler<GetAllEmployeesQuery, IList<Employee>>, GetAllEmployeesQueryHandler>();
+builder.Services.AddScoped<IQueryHandler<GetAllMaterialsQuery, IList<Material>>, GetAllMaterialsQueryHandler>();
+builder.Services.AddScoped<IQueryHandler<GetAllPlacesQuery, IList<Place>>, GetAllPlacesQueryHandler>();
 
 var app = builder.Build();
 
