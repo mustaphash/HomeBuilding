24f136b92d15bc19c08dbca196f67bf038bddd21
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-04 23:56:44 +0200

    Add GetAllEMployees Query and QueryHandler

---
+++ src/DAL/Queries/GetAllEmployees/GetAllEmployeesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entity;
+using Core.Queries;
+
+namespace DAL.Queries.GetAllEmployees
+{
+    public class GetAllEmployeesQuery : IQuery<IList<Employee>>
+    {
+    }
+}
---
+++ src/DAL/Queries/GetAllEmployees/GetAllEmployeesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entity;
+using Core.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Queries.GetAllEmployees
+{
+    public class GetAllEmployeesQueryHandler : IQueryHandler<GetAllEmployeesQuery, IList<Employee>>
+    {
+        private readonly BuildingContext _buildingContext;
+        public GetAllEmployeesQueryHandler(BuildingContext buildingContext)
+        {
+            _buildingContext = buildingContext;
+        }
+        public async Task<IList<Employee>> HandleAsync(GetAllEmployeesQuery query, CancellationToken cancellationToken = default)
+        {
+            List<Employee> employees = await _buildingContext.Employees.ToListAsync(cancellationToken);
+
+            return employees;
+        }
+    }
+}
