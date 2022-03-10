6ebf62ac3571692441a9454581de88db8300c659
Mustafa Shakir <mshakir9918@gmail.com>
2022-03-10 17:35:13 +0200

    Add HttpPost to EmployeeController

--- src/HomeBuild/Controllers/EmployeeController.cs
+++ src/HomeBuild/Controllers/EmployeeController.cs
@@ -1,6 +1,9 @@
-﻿using Core.Entity;
+﻿using Core.Commands;
+using Core.Entity;
 using Core.Queries;
+using DAL.Commands;
 using DAL.Queries.GetAllEmployees;
+using HomeBuild.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeBuild.Controllers
@@ -10,18 +13,30 @@ namespace HomeBuild.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly IQueryHandler<GetAllEmployeesQuery, IList<Employee>> _getAllEmployeesQuery;
+        private readonly ICommandHandler<CreateEmployeeCommand> _createEmployeeCommand;
         public EmployeeController(
-            IQueryHandler<GetAllEmployeesQuery, IList<Employee>> getAllEmployeesQuery)
+            IQueryHandler<GetAllEmployeesQuery, IList<Employee>> getAllEmployeesQuery,
+            ICommandHandler<CreateEmployeeCommand> createEmployeeCommand)
         {
             _getAllEmployeesQuery = getAllEmployeesQuery;
+            _createEmployeeCommand = createEmployeeCommand;
         }
 
-        [HttpGet(Name = "GetAllEmployees")]
+        [HttpGet]
         public async Task<IActionResult> GetAllEmployees()
         {
             IList<Employee> employees = await _getAllEmployeesQuery.HandleAsync(new GetAllEmployeesQuery());
 
             return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(EmployeeModel model)
+        {
+            var employee = model.ToEmployee();
+            await _createEmployeeCommand.HandleAsync(new CreateEmployeeCommand(employee));
+
+            return NoContent();
         }
     }
 }
