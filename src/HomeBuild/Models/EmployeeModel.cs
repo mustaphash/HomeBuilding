1864bd2e8f338b8dcb0e60c23955f5a5a471208e
Mustafa Shakir <mshakir9918@gmail.com>
2022-03-10 17:28:13 +0200

    Create Employee Model

---
+++ src/HomeBuild/Models/EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entity;
+
+namespace HomeBuild.Models
+{
+    public class EmployeeModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public Employee ToEmployee()
+        {
+            return new Employee()
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Adress = Adress,
+                PhoneNumber = PhoneNumber
+            };
+        }
+    }
+}
